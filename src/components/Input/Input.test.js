5594a0f80eee3bd240f890d4430631b1e80c6c1b
raosan <raosanf.lillahi@gmail.com>
2019-02-11 11:57:16 +0700

    test props success true

--- src/components/Input/Input.test.js
+++ src/components/Input/Input.test.js
@@ -4,16 +4,24 @@ import { shallow } from 'enzyme'
 import { findByTestAttr, storeFactory } from '../../../test/testUtils'
 import { Input } from './Input'
 
-const setup = (initialState={}) => {
-	const store = storeFactory(initialState)
-	const wrapper = shallow(<Input store={store} />)
-	return wrapper
+// const setup = (initialState={}) => {
+// 	const store = storeFactory(initialState)
+// 	const wrapper = shallow(<Input {...store} />)
+// 	console.log(wrapper.debug())
+// 	return wrapper
+// }
+
+const defaultProps = { success: false }
+
+const setup = (props={}) => {
+	const setupProps = { ...defaultProps, ...props }
+	return shallow (<Input {...setupProps} />)
 }
 
 describe('render', () => {
 	let wrapper
 	beforeEach(() => {
-		const initialState = {success: true}
+		const initialState = {success: false}
 		wrapper = setup(initialState)
 	})
 
@@ -33,14 +41,23 @@ describe('render', () => {
 	})
 
 	describe('word has been guessed', () => {
-		test('render without error', () => {
+		let wrapper
+		beforeEach(() => {
+			const initialState = {success: true}
+			wrapper = setup(initialState)
+		})
 
+		test('render without error', () => {
+			const component = findByTestAttr(wrapper, 'component-input')
+			expect(component.length).toBe(1)
 		})
 		test('not render input box', () => {
-			
+			const inputBox = findByTestAttr(wrapper, 'input-box')
+			expect(inputBox.length).toBe(0)
 		})
 		test('not render submit button', () => {
-			
+			const submitButton = findByTestAttr(wrapper, 'submit-button')
+			expect(submitButton.length).toBe(0)
 		})
 	})
 })
